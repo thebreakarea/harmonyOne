56c882e27a93707d487ae9a8867d170b959391c6
thebreakarea <95386779+thebreakarea@users.noreply.github.com>
2022-01-20 00:11:50 +0100

    Update restart_service.sh
    Added golden value, so no need to run the entire script if the situation does not change.

--- .github/restart_service.sh
+++ .github/restart_service.sh
@@ -18,7 +18,19 @@ signed_first_check=$(/home/harmony/hmy --node="https://api.s0.t.hmny.io" blockch
 #Dumping the result on the log file and on a dat file
 echo "On the first check there were signed: " $signed_first_check >> /home/harmony/log_signature_check_$(date +"%Y_%m_%d").log
 echo $signed_first_check > /home/harmony/signed_first_check.dat
-
+first_signed_difference=$((to_sign_first_check - signed_first_check))
+Golden_File=/home/harmony/to_sign_golden_reference.dat
+    if [ -f "$Golden_File" ]; then
+        golden_value=`cat /home/harmony/to_sign_golden_reference.dat`
+               if [ $first_signed_difference -eq $golden_value ]; then
+                   echo "*#*#*#*#*#*#*#*" >> /home/harmony/log_signature_check_$(date +"%Y_%m_%d").log
+                   echo "Difference matches the Golden value, no need to continue " $first_signed_difference>> /home/harmony/log_signature_check_$(date +"%Y_%m_%d").log
+                   echo "No action are needed at this time.">> /home/harmony/log_signature_check_$(date +"%Y_%m_%d").log
+                   echo "*#*#*#*#*#*#*#*" >> /home/harmony/log_signature_check_$(date +"%Y_%m_%d").log
+                   echo -e "\n\n" >> /home/harmony/log_signature_check_$(date +"%Y_%m_%d").log
+                   exit
+               fi
+   fi
 #Checking the difference, if different from zero take further actions.
 if [ $to_sign_first_check -ge $signed_first_check ]; then
             first_signed_difference=$((to_sign_first_check - signed_first_check))
@@ -38,6 +50,11 @@ if [ $to_sign_first_check -ge $signed_first_check ]; then
             echo "On the second check there were signed: " $signed_second_check >> /home/harmony/log_signature_check_$(date +"%Y_%m_%d").log
             echo $signed_second_check > /home/harmony/signed_second_check.dat
             second_signed_difference=$((to_sign_second_check - signed_second_check))
+            
+            if [$first_signed_difference -eq $second_signed_difference ]; then
+            echo $first_signed_difference > /home/harmony/to_sign_golden_reference.dat
+            fi
+            
             echo "Second difference: " $second_signed_difference>> /home/harmony/log_signature_check_$(date +"%Y_%m_%d").log
             total_difference=$((second_signed_difference - first_signed_difference))
                     if [ $total_difference -ge 5 ]; then   
