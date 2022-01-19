eaaab6dfd316e5f0ca0feb8d28569520a2910e39
thebreakarea <95386779+thebreakarea@users.noreply.github.com>
2022-01-19 23:31:40 +0100

    Update restart_service.sh

--- .github/restart_service.sh
+++ .github/restart_service.sh
@@ -17,11 +17,12 @@ signed_first_check=$(/home/harmony/hmy --node="https://api.s0.t.hmny.io" blockch
 
 #Dumping the result on the log file and on a dat file
 echo "On the first check there were signed: " $signed_first_check >> /home/harmony/log_signature_check_$(date +"%Y_%m_%d").log
-echo $signed_first_check /home/harmony/signed_first_check.dat
+echo $signed_first_check > /home/harmony/signed_first_check.dat
 
 #Checking the difference, if different from zero take further actions.
 if [ $to_sign_first_check -ge $signed_first_check ]; then
             first_signed_difference=$((to_sign_first_check - signed_first_check))
+            echo "First difference: " $first_signed_difference>> /home/harmony/log_signature_check_$(date +"%Y_%m_%d").log
             sleep 60
             #Getting the number of blocks requested to be signed after 1 minute
             to_sign_second_check=$(/home/harmony/hmy --node="https://api.s0.t.hmny.io" blockchain validator information one192wqdhlk84lvxmrd6jl9l9njy7f94q4a3hd87w | grep current-epoch-to-sign | awk 'NR==1 { print$2}' | sed 's/,//') 
@@ -35,14 +36,36 @@ if [ $to_sign_first_check -ge $signed_first_check ]; then
             
             #Dumping the result on the log file and on a dat file
             echo "On the second check there were signed: " $signed_second_check >> /home/harmony/log_signature_check_$(date +"%Y_%m_%d").log
-            echo $signed_second_check /home/harmony/signed_second_check.dat
+            echo $signed_second_check > /home/harmony/signed_second_check.dat
             second_signed_difference=$((to_sign_second_check - signed_second_check))
+            echo "Second difference: " $second_signed_difference>> /home/harmony/log_signature_check_$(date +"%Y_%m_%d").log
                 if [ $second_signed_difference -ge $first_signed_difference ]; then
                 total_difference=$((second_signed_difference - first_signed_difference))
-                    if [ $total_difference -ge 5 ]; then                 
-                        echo "Harmony service restarted on: " $(date)>> /home/harmony/log_signature_check_$(date +"%Y_%m_%d").log
-                        sudo systemctl restart harmony.service
+                    if [ $total_difference -ge 5 ]; then   
+                    FILE=/home/harmony/restart_number_$(date +"%Y_%m_%d_%H").dat
+                          if [ -f "$FILE" ]; then
+                                restart_times=`cat /home/harmony/restart_number_$(date +"%Y_%m_%d_%H")`
+                                      if [ $restart_times -lt 6 ]; then
+                                      echo "Harmony service restarted on: " $(date)>> /home/harmony/log_signature_check_$(date +"%Y_%m_%d").log
+                                      sudo systemctl restart harmony.service
+                                      restart_times=$((restart_times+1))
+                                      echo $restart_times > /home/harmony/restart_number_$(date +"%Y_%m_%d_%H").dat
+                                      else 
+                                      EOF
+                                      fi
+                            else 
+                                echo 1 > /home/harmony/restart_number_$(date +"%Y_%m_%d_%H").dat
+                                #Remove old file
+                                rm -f /home/harmony/restart_number_$(date -d '1 hour ago' +"%Y_%m_%d_%H").dat
+                                echo "Harmony service restarted on: " $(date)>> /home/harmony/log_signature_check_$(date +"%Y_%m_%d").log
+                                sudo systemctl restart harmony.service
+                                fi              
+                        
                     fi
+                else
+                echo "Total difference: " $total_difference>> /home/harmony/log_signature_check_$(date +"%Y_%m_%d").log
+                echo "No action are needed at this time.>> /home/harmony/log_signature_check_$(date +"%Y_%m_%d").log
                 fi
+                
 fi
 EOF
